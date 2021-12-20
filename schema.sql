61e2864e506afd070aae7fa670771d450f82859d
zinashah8 <zina.shah8@gmail.com>
2021-12-19 21:46:02 -0500

    updated schema with preliminary table creation

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
+-- Created Database
 CREATE DATABASE COVID_Vaccination_Study
+
+-- Created Table containing preliminary data for COVID Vaccination study
+
+CREATE TABLE Vaccination_Data 
+	(state_name varchar, 
+	 county_name varchar,
+	 num_vaccinations float, 
+	 per_capita_income float, 
+	 sociodemographic_barriers varchar, 
+	 health_access_barriers varchar
+	);
