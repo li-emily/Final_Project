620673386797d145b4609f26b8112132bf5735cf
zinashah8 <zina.shah8@gmail.com>
2021-12-19 22:37:51 -0500

    updated schema and added screenshot of per_capita_income table

--- schema.sql
+++ schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE Vaccination_Data
 	 health_access_barriers varchar
 	);
 
+-- Created table for per capita income information of all US counties 
 CREATE TABLE per_capita_income 
     (county_name varchar, 
     income_2018 float, 
@@ -19,7 +20,11 @@ CREATE TABLE per_capita_income
     income_2020 float
     );
 
+-- Inserted data into per_capita_income table 
 COPY per_capita_income_counties(county, income_2018, income_2019, income_2020)
 FROM '/Users/zina/Desktop/Final Project Data/per_capita_income_counties.csv'
 DELIMITER ','
 CSV HEADER;    
+
+-- Viewed newly created table with data inserted
+SELECT * FROM per_capita_income
